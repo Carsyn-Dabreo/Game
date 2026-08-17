781af0c885d0b48c66dcd7c7cb31922bb103ba75
Carsyn-Dabreo <151903093+Carsyn-Dabreo@users.noreply.github.com>
2026-08-17 12:35:48 +0530

    Switch app to Vehicle Lab 3D experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import CityGame from './components/game/CityGame'
+import VehicleLab from './components/vehicle/VehicleLab'
 
-function App() {
-  return (
-    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
-      <CityGame />
-    </div>
-  )
+export default function App() {
+  return <VehicleLab />
 }
-
-export default App
